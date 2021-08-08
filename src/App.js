ffde3a9aea99a767e6ca823c4750dde5839bc8d9
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-08-08 09:29:01 +0530

    initial folder structure and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import CartScreen from "./Screens/CartScreen/CartScreen";
+import HomeScreen from "./Screens/HomeScreen/HomeScreen";
+import NavBar from "./Screens/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SaveLater from "./Screens/SaveLaterScreen/SaveLater";
 
 function App() {
   return (
     <div>
-    <h1>Hello</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/cart" element={<CartScreen />} />
+          <Route path="/saved" element={<SaveLater />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Screens/SaveLaterScreen/SaveLater.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SaveLater() {
+  return <div>SaveLater</div>;
+}
+
+export default SaveLater;
