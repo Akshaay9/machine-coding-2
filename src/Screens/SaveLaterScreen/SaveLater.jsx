d2a73223c079ff347ce921bc49bfe738a3e0f5f5
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-08-08 12:32:26 +0530

    feat:save later page implemented

--- src/Screens/SaveLaterScreen/SaveLater.jsx
+++ src/Screens/SaveLaterScreen/SaveLater.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import { useSaveLaterContext } from "../../Context/SaveLaterContext/SaveLaterContext";
+import Button from "@material-ui/core/Button";
 function SaveLater() {
-  return <div>SaveLater</div>;
+  const {
+    state: { saveLater },
+    saveDispatch,
+  } = useSaveLaterContext();
+
+  return (
+    <div>
+      <div className="heading">
+        {" "}
+        <h3>Saved Later</h3>{" "}
+      </div>
+      <div className="product-container cart-container">
+        {saveLater.map((ele) => (
+          <div className="individualProduct">
+            <div className="product-container">
+              <img src={ele.images[0].img} alt="" />
+              <div className="product-desc">
+                <h5>{ele.name}</h5>
+                <p>₹{ele.price}</p>
+              </div>
+              <div className="product-container-CTA">
+                <div style={{ display: "flex" }}>
+                  <h5>Brand</h5> : <p>{ele.tag}</p>
+                </div>
+              </div>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() =>
+                  saveDispatch({ type: "SAVE_LATER", payload: ele })
+                }
+              >
+                Remove from Save Later
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SaveLater;
